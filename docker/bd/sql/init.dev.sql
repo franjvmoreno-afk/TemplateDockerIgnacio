654f194d2d9837e04bfbb1ef938f52a8d5e8cf37
Franjvmoreno <franjvmoreno@gmail.com>
2026-01-08 08:57:01 +0100

    init.dev.sql con comentarios

--- docker/bd/sql/init.dev.sql
+++ docker/bd/sql/init.dev.sql
@@ -1,3 +1,5 @@
-CREATE SCHEMA IF NOT EXISTS `template_docker_app_dev` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE SCHEMA IF NOT EXISTS `template_docker_app_dev`
+DEFAULT CHARACTER SET utf8mb4            -- Soporte completo de caracteres
+COLLATE utf8mb4_general_ci;              -- Comparación estándar y rápida
 
-USE `template_docker_app_dev`;
+USE `template_docker_app_dev`;            -- Selecciona el esquema para trabajar
